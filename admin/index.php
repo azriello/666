46c9da8b22aa6b45f3580071c2e1e171123bb054
azriello <azrielataya121@gmail.com>
2020-06-06 14:14:11 +0700

    menambahkan file bernama index.php dan logout.php di folder admin

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Cara Membuat Form Login Dengan PHP MySQLi - Tulisan IKO</title>
+</head>
+<body>
+ <h2>Halaman Admin</h2>
+
+ <br />
+ <!-- cek apakah sudah login -->
+ <?php 
+ session_start();
+ if($_SESSION['status']!="login"){
+  header("location:../index.php?pesan=belum_login");
+ }
+ ?>
+ <h4>Selamat datang, <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
+ <br />
+ <a href="logout.php">LOGOUT</a>
+</body>
+</html>
